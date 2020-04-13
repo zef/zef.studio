40dd913d0655386e2860d2b41ac754df65c66205
Zef Houssney <zef@madebykiwi.com>
2020-04-13 17:38:36 -0600

    Install SassPublishPlugin.

--- Package.swift
+++ Package.swift
@@ -8,12 +8,13 @@ let package = Package(
         .executable(name: "Studio", targets: ["Studio"])
     ],
     dependencies: [
-        .package(url: "https://github.com/johnsundell/publish.git", from: "0.3.0")
+        .package(url: "https://github.com/johnsundell/publish.git", from: "0.3.0"),
+        .package(url: "https://github.com/hejki/sasspublishplugin", from: "0.1.0")
     ],
     targets: [
         .target(
             name: "Studio",
-            dependencies: ["Publish"]
+            dependencies: ["Publish", "SassPublishPlugin"]
         )
     ]
 )
