208ff530aca96cd92b5f4ca55bd23cd27258f083
Zef Houssney <zef@madebykiwi.com>
2021-02-04 17:37:12 -0700

    Commit package changes I made a long time ago.

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
         .executable(name: "Studio", targets: ["Studio"])
     ],
     dependencies: [
-        .package(url: "https://github.com/johnsundell/publish.git", from: "0.3.0"),
+        .package(url: "https://github.com/johnsundell/publish.git", from: "0.7.0"),
         .package(url: "https://github.com/hejki/sasspublishplugin", from: "0.1.0")
     ],
     targets: [
