1eff4334a3933c9096c3c8ff18452744680ccb0d
Zef Houssney <zef@madebykiwi.com>
2021-02-11 11:18:34 -0700

    Fix mistake of using brackets instead of parens for image URL.

--- Sources/Studio/ImageFormatModifier.swift
+++ Sources/Studio/ImageFormatModifier.swift
@@ -25,28 +25,53 @@ public extension Modifier {
     Modifier(target: .images) { html, markdown in
 
       // parse out the text and image path from the markddown
-      let elements = String(markdown).deletingPrefix("![").deletingSuffix("]").components(separatedBy: "][")
+      let elements = String(markdown).deletingPrefix("![").deletingSuffix(")").components(separatedBy: "](")
       guard elements.count == 2, let text = elements.first, let path = elements.last else {
         fatalError("Unexpected format found when parsing image markdown...")
       }
 
       let captionData = CaptionParser.parse(text)
-      let figure: Node<HTML.BodyContext> = .figure(path, alt: captionData.alt, caption: captionData.caption)
+      let figure: Node<HTML.BodyContext> = .figure(path, alt: captionData.alt, caption: captionData.caption, classes: captionData.htmlClass)
 
       return figure.render()
     }
   }
+
+
+  static func formatImageLists() -> Self {
+    return Modifier(target: .lists) { html, markdown in
+      return html.replacingOccurrences(of: "<ul><li><figure", with: "<ul class='image-list'><li><figure")
+    }
+  }
 }
+
+// Here we can take a string that is entered where the "alt" tag is normally placed in markdown
+// and parse out some additional data that we want to include.
+//
+// Only use alt text:
+// ![Some Alt Text][image_path.jpg]
+//
+// Use alt text, and duplicate that text as the "caption" by including the delimiter character,
+// and an ampersand to indicate the duplication:
+// ![Some Alt Text |&][image_path.jpg]
+//
+// Use alt text, and separate text as the "caption" by using the delimiter character followed by caption text:
+// ![Some Alt Text | Some caption text.][image_path.jpg]
+//
+// Finally, including ".class-name" at the beginning of the alt text will return that as a class string
+// to be inserted in the HTML of the figure tag. You can include multiple classes with no space like this ".class-one.class-two"
+// ![.some-class Some Alt Text][image_path.jpg]
+
 
 struct CaptionParser {
   static var delimiter = "|"
   static var useAltAsCaptionIndicator = "&"
 
-  typealias CaptionData = (alt: String, caption: String?)
+  typealias CaptionData = (alt: String, caption: String?, htmlClass: String?)
 
   static func parse(_ text: String) -> CaptionData {
     var textElements = text.components(separatedBy: delimiter)
-    let alt = textElements.removeFirst().trimmingCharacters(in: .whitespaces)
+    var alt = textElements.removeFirst().trimmingCharacters(in: .whitespaces)
 
     guard textElements.count <= 1 else {
       fatalError("Unexpected number of elements found when parsing caption. Too many delimiters.")
