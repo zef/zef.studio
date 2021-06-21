d5edde31509f9ffad5d4570aedcec16363858ecc
Zef Houssney <zef@madebykiwi.com>
2021-06-21 09:47:15 -0600

    Update favicons and related metadata.

--- Sources/Studio/StudioTheme.swift
+++ Sources/Studio/StudioTheme.swift
@@ -314,13 +314,17 @@ public extension Node where Context == HTML.HeadContext {
 <link rel="apple-touch-icon" sizes="144x144" href="/images/favicons/apple-icon-144x144.png">
 <link rel="apple-touch-icon" sizes="152x152" href="/images/favicons/apple-icon-152x152.png">
 <link rel="apple-touch-icon" sizes="180x180" href="/images/favicons/apple-icon-180x180.png">
+<link rel="mask-icon" href="/images/favicons/safari-pinned-tab.svg" color="#375686">
 <link rel="icon" type="image/png" sizes="192x192"  href="/images/favicons/android-icon-192x192.png">
-<link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="96x96" href="/images/favicons/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/images/favicons/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="/images/favicons/favicon-16x16.png">
-<meta name="msapplication-TileColor" content="#ffffff">
-<meta name="msapplication-TileImage" content="/images/favicons/ms-icon-144x144.png">
+<link rel="manifest" href="/images/favicons/site.webmanifest">
+<meta name="msapplication-TileColor" content="#375686">
+<meta name="msapplication-config" content="/images/favicons/browserconfig.xml">
 <meta name="theme-color" content="#ffffff">
+<meta name="application-name" content="zef.studio">
+<meta name="apple-mobile-web-app-title" content="zef.studio">
 """.replacingOccurrences(of: "\n", with: "")
     )
   }
